6c5fa64590690d3d935880dc60b6ae6004575d5e
Kuroky-Chen <5837907@qq.com>
2022-03-25 18:03:42 +0800

    feat: add module

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue';
-// 试一试husky
-let cc = ref('Kuroky');
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+// import { ref } from 'vue'
+// 试一试husky
+// let cc = ref('Kuroky')
+</script>
 
 <style>
 #app {
